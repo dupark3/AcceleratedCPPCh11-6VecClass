f6ab122e482db93a0ed180b3b3cb911e441c416c
Du Park <dupark3@gmail.com>
2018-03-08 13:28:50 -0500

    Copy constructor, size, begin, end, and operator member functions defined.

--- Vec.h
+++ Vec.h
@@ -12,10 +12,27 @@ public:
     typedef T& reference;
     typedef const T& const_reference;
 
+    // constructors
     Vec() { create(); }  // default constructor, called by Vec<int> v;
     explicit Vec(size_t n, const T& val = T()) { create(n, val); } // constructor takes size_t and/or const T (default argument value initialized)
+    Vec(const Vec& v) { } // copy constructor
+    
+    // operator[] index accessor
+    T& operator[](size_t i) { return data[i]; }
+    // reference operator[](size_type i) { return *(data + i); }
+    const T& operator[](size_type i) const { return data[i]; }
+    
+    // size() accessor
+    size() const { return limit - data; }
+    
+    // begin/end accessors
+    iterator begin() { return data; }
+    const_iterator begin() const { return data; }
+    iterator end() { return limit; }
+    const_iterator end() const { return limit; }
 
-    Vec(size_t, T); // argument constructor takes number of elements and a value. Initializes all elements to given value
+
+
 private:
     iterator data; // points to the first element
     iterator limit; // points to (one past) the last element
