53a03fe293809c3d425389d27ccda9285c3692f6
Du Park <dupark3@gmail.com>
2018-03-09 07:20:30 -0500

    Added private member functions grow and unchecked_append for supporting push_back

--- Vec.cpp
+++ Vec.cpp
@@ -21,13 +21,13 @@ template <class T>
 void Vec<T>::create(size_t n, const T& val){
     data = alloc.allocate(n);
     limit = avail = data + n;
-    uninitialized_fill(data, limit, val);
+    std::uninitialized_fill(data, limit, val);
 }
 
 template <class T>
 void Vec<T>::create(const_iterator first, const_iterator end){
     data = alloc.allocate(end - first);
-    limit = avail = uninitialized_copy(first, end, data);  
+    limit = avail = std::uninitialized_copy(first, end, data);
 }
 
 template <class T>
@@ -45,3 +45,25 @@ void Vec<T>::uncreate(){
     data = avail = limit = 0;
 }
 
+template <class T>
+void Vec<T>::grow(){
+    // grow twice as much as space currently in use, or if empty, by one
+    size_type new_size = std::max(2 * (limit - data), ptrdiff_t(1));
+
+    // allocate new space and copy existing elements to the new space
+    iterator new_data = alloc.allocate(new_size);
+    iterator new_avail = avail = std::uninitalized_copy(data, avail, new_data);
+
+    // return old space
+    uncreate();
+
+// reset pointers to the newly allocated space
+    data = new_data;
+    avail = new_avail;
+    limit = data + new_size;
+}
+
+template <class T>
+void Vec<T>::unchecked_append(const T& val){
+    alloc.construct(avail++, val);
+}
